1b11c1a3551110a801168649a77059673f552b2e
Georgiy Sheynberg <you@example.com>
2025-05-17 15:20:53 +0300

    access_token model

--- study_platform/backend/core/models/base.py
+++ study_platform/backend/core/models/base.py
@@ -1,7 +1,7 @@
 from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
-from core.config import settings
+from study_platform.backend.core.config import settings
 from study_platform.backend.utils import camel_case_to_snake_case
 
 
@@ -9,12 +9,9 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     metadata = MetaData(
-        naming_convention=settings.db.naming_conventions
+        naming_convention=settings.db.naming_convention
     )
 
     @declared_attr.directive
     def __tablename__(cls):
-        return f'{camel_case_to_snake_case(cls.__name__)}'
-
-
-    id: Mapped[int] = mapped_column(primary_key=True)
+        return f'{camel_case_to_snake_case(cls.__name__)}s'
